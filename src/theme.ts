bb6bcb81d5fe10d0e1c6c7a6913c07e710fa5f5d
delberss <delberss@hotmail.com>
2025-10-27 21:29:33 -0300

    chore: adiciona cor de contraste nos temas

--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,8 @@ import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 let theme = createTheme({
     palette: {
-        primary: { main: '#232323' },
-        secondary: { main: '#4f8e3e' },
+        primary: { main: '#232323', contrastText: '#ffffff'},
+        secondary: { main: '#4f8e3e', contrastText: '#000000ff' },
     },
     typography: {
         fontFamily: "Segoe UI",
