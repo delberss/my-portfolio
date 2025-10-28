d7db7724ee33936baf345196b7408baa1a6b6e1d
delberss <delberss@hotmail.com>
2025-10-27 21:33:18 -0300

    feat: adiciona AboutSection e SkillsSection no projeto

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
 import NavBar from "../../components/NavBar/NavBar"
+import AboutSection from "./sections/AboutSection/AboutSection"
 import Hero from "./sections/Hero/Hero"
+import SkillsSection from "./sections/SkillsSection/SkillsSection"
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <AboutSection />
+      <SkillsSection />
     </>
   )
 }
